625de329f13238f49447182faf385c3cc4f0cd04
Brody Sears <brodysears@Brodys-Mac-mini.local>
2022-01-19 13:38:43 -0700

    First Commit

---
+++ Garage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Garage
+//
+//  Created by Brody Sears on 1/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
